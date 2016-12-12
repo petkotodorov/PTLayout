0d4963f4cf8be88fc54d1349962b7810fe09c577
inforadmin <ptodorov@czpamptodorov1.infor.com>
2016-12-12 09:26:12 +0100

    Initial Commit

---
+++ PTLayout/PTLayout.h
@@ -0,0 +1,19 @@
+//
+//  PTLayout.h
+//  PTLayout
+//
+//  Created by Petko Todorov on 12/12/16.
+//  Copyright © 2016 Petko Todorov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PTLayout.
+FOUNDATION_EXPORT double PTLayoutVersionNumber;
+
+//! Project version string for PTLayout.
+FOUNDATION_EXPORT const unsigned char PTLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PTLayout/PublicHeader.h>
+
+
